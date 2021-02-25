017a690d3a012181ae530192ed51c90521eef3f7
athmekr <athmekr@gmail.com>
2021-02-25 10:57:45 +0200

    Created Home, Header, Landmarks, Login, Signup components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LandmarksComponent } from './components/landmarks/landmarks.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LandmarksComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
