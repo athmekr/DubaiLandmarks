ec4fccdd55e634bb68cffe66364319049cc09ee0
athmekr <athmekr@gmail.com>
2021-02-28 19:12:58 +0200

    Login component improvement

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+//import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit, OnDestroy {
 
-  ngOnInit(): void {
+  private authStatusSub: Subscription;
+
+  //constructor(public authService: AuthService) { }
+
+  ngOnInit() {
+/*     this.authStatusSub = this.authService.getStatusListener().subscribe(
+      authStatus => {
+        this.isLoading = false;
+      }
+    ); */
+   }
+
+  onLogin (form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    //this.authService.login(form.value.email, form.value.password);
   }
 
+  ngOnDestroy(){
+    this.authStatusSub.unsubscribe();
+  }
 }
