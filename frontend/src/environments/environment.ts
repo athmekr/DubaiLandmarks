7335355905f3dca2e66803d2088c75a056cff906
athmekr <athmekr@gmail.com>
2021-03-01 16:27:09 +0200

    Updated environment variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,8 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  PARSE_SERVER: {
+      SERVER_URL: 'http://localhost:5000/parse',
+      APP_ID: 'Instaland'
+  },
+  LANDMARKS_API_URL: 'http://localhost:5000/api/landmarks'
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
