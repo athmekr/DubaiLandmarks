ef2cc542c9f44135a3ad6b3dbbdb3a1a38e42979
athmekr <athmekr@gmail.com>
2021-03-04 12:37:53 +0200

    Updated landmarks routes

--- backend/routes/landmarks.js
+++ backend/routes/landmarks.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const LandmarksController = require('../controllers/landmarks');
 const router = express.Router();
-const checkAuth = require('../middleware/check-auth');
+//const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/file');
 
+// Get all landmarks
 router.get('/', LandmarksController.getLandmarks);
 
+// Get landmark by id
 router.get('/:id', LandmarksController.getLandmark);
-/*
-router.put('/:id', checkAuth, LandmarksController.updateLandmark);
 
-router.post('', checkAuth, LandmarksController.createLandmark);
- */
+// Update landmark
+router.put('/:id', extractFile, LandmarksController.updateLandmark);
+
 module.exports = router;
