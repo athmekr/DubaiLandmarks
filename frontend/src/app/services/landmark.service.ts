6f75892865f16be2b2d93cde0c92d3d344380a9e
athmekr <athmekr@gmail.com>
2021-03-05 17:31:43 +0200

    landmark service changes

--- frontend/src/app/services/landmark.service.ts
+++ frontend/src/app/services/landmark.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, Subject  } from 'rxjs';
+import { BehaviorSubject, Observable, Subject  } from 'rxjs';
 import { Router } from '@angular/router';
 import { Landmark } from "../models/landmark.model";
 import { AuthService } from "./auth.service";
 import { environment } from "../../environments/environment";
+import Parse from 'parse';
 
 
 @Injectable({
@@ -13,27 +14,33 @@ import { environment } from "../../environments/environment";
 export class LandmarkService {
 
   private landmark_url = environment.LANDMARKS_API_URL;
+  public isToggled: BehaviorSubject<boolean>;
 
-  constructor(private httpClient: HttpClient, private authService: AuthService) {}
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
+    this.isToggled = new BehaviorSubject<boolean>(false);
+  }
 
   // GET landmarks
   getLandmarks(): Observable<Landmark[]> {
-    return this.httpClient.get<Landmark[]>(this.landmark_url);
+    return this.httpClient.get<Landmark[]>(this.landmark_url/* , {headers: this.generateHeaderToken()} */);
   }
 
   // GET landmark by id
   getLandmarkById(landmarkId: string): Observable<Landmark> {
     const url = `${this.landmark_url}/${landmarkId}`;
-    return this.httpClient.get<Landmark>(url);
+    return this.httpClient.get<Landmark>(url/* , {headers: this.generateHeaderToken()} */);
   }
 
   // Update landmark
   updateLandmark(landmark: Landmark): Observable<Landmark> {
     const formData = new FormData();
+    console.log('update111');
     formData.append('title', landmark.title);
+    console.log(landmark.title);
+
     formData.append('description', landmark.description);
-    formData.append('url', landmark.url);
-    formData.append('photo', landmark.image_file);
+    //formData.append('url', landmark.url);
+    //formData.append('photo', landmark.image_file);
 
     const url = `${this.landmark_url}/${landmark.objectId}`;
     return this.httpClient.put<Landmark>(url, formData, {headers: this.generateHeaderToken()});
@@ -44,4 +51,5 @@ export class LandmarkService {
       'X-Parse-Session-Token': this.authService.getToken,
     });
   }
+
 }
